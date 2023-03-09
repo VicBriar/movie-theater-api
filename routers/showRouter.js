7cbeb57abd1068710deedd049a0023226bd45344
Victoria McCrady <spartanginger@gmail.com>
2023-03-09 09:16:13 -0800

    build(userrouter, showrouter, server): showrouter & userRouter connect to server file

---
+++ routers/showRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const {body, validationResult} = require('express-validator');
+//in this project, our instance of sequelize is called 'db'
+const {db} = require('../db'); 
+const {Show} = require('../models/index')
+
+//GET
+
+//POST
+
+//PUT
+
+//DELETE
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 //routers
+const userRouter = require('./routers/userRouter');
+const showRouter = require('./routers/showRouter')
 
 
 //routes
+app.use('/shows',showRouter)
+app.use('/users',userRouter)
+
 
 //server listening
 app.listen(port, () =>{
