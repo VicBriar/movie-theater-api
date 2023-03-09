d4afdeed46927e72316f35a062d723bae1b47d2a
Victoria McCrady <spartanginger@gmail.com>
2023-03-09 09:26:07 -0800

    build(servers.js): give server.js parsing functionality

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const app = express()
 const port = 3000
 //routers
 const userRouter = require('./routers/userRouter');
-const showRouter = require('./routers/showRouter')
+const showRouter = require('./routers/showRouter');
 
+//parsing
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 //routes
-app.use('/shows',showRouter)
-app.use('/users',userRouter)
+app.use('/shows',showRouter);
+app.use('/users',userRouter);
 
 
 //server listening
 app.listen(port, () =>{
-    console.log(`app is listening at HTTP://localhost${port}`)
+    console.log(`app is listening at HTTP://localhost${port}`);
 })
